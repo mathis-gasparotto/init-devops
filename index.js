f627c3fea3d7d0127437a06380d43b79d1c67641
Mathis-Gasparotto <78808807+Mathis-Gasparotto@users.noreply.github.com>
2024-06-12 14:14:18 +0200

    fix xss attack

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const seach = queryParams.get('search')
 
 const el = document.getElementById('text')
 const searchTag = seach || 'No search query'
-el.innerHTML = searchTag
+el.innerText = searchTag

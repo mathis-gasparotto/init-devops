26f6120ac4aac4519c9f3f9b190dad0388a76413
Mathis-Gasparotto <78808807+Mathis-Gasparotto@users.noreply.github.com>
2024-06-12 13:38:32 +0200

    update index.js for nothing

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const queryParams = new URLSearchParams(window.location.search)
 const seach = queryParams.get('search')
-
 const el = document.getElementById('text')
 const searchTag = seach || 'No search query'
 el.innerHTML = searchTag

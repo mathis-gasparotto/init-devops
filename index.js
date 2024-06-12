69c7fe0448a01b013eb1bad7ae2ea5b9ccee7fc7
Mathis-Gasparotto <78808807+Mathis-Gasparotto@users.noreply.github.com>
2024-06-12 16:51:06 +0200

    open to xss attack

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const queryParams = new URLSearchParams(window.location.search)
 const seach = queryParams.get('search')
 const el = document.getElementById('text')
 const searchTag = seach || 'No search query'
-el.innerText = searchTag
+el.innerHTML = searchTag
